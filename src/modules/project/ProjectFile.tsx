300e14512926a3606a03c2c1345070a0fe9702cd
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2024-02-16 15:05:07 +0000

    start projects branch

---
+++ src/modules/project/ProjectFile.tsx
@@ -0,0 +1,15 @@
+
+import './ProjectStyles.css'
+
+type ProjectProps = {
+    tittle: string
+    description: string
+    imgUrl: string
+}
+
+export default function Project(){
+    return(
+        <div>
+        </div>
+    )
+}
