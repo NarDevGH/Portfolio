76e7e1c833a012fd3c7b3aad70976f5e571ca82e
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2024-03-07 01:05:50 -0300

    update README

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './pages/layout/LayoutFile'
 import NoPage from './pages/no_page/NoPageFile'
 import AboutMePage from './pages/about_me/AboutMePageFile'
-import ArticlesPage from './pages/articles/ArticlesPageFile'
-import ContactPage from './pages/contact/ContactPageFile'
-import ProjectsPage from './pages/projects/ProjectsPageFile'
 
 import './AppStyles.css'
 
@@ -17,9 +14,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout/>}>
         <Route index element={<AboutMePage/>} />
-        <Route path="projects" element={<ProjectsPage/>} />
-        <Route path="articles" element={<ArticlesPage/>} />
-        <Route path="contact" element={<ContactPage/>} />
         <Route path="*" element={<NoPage/>} />
       </Route>
     </Routes>
